b51f2c6e1f5db4527c3fc6259a8d5270a11f4f3a
Harry Ferrier <f3rr13r@hotmail.com>
2016-11-23 11:26:07 +0000

    Push files to github

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,53 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    // Hook up the an object for the UITableView class in our Main.storyboard. This means that we can reload it's data..
+    @IBOutlet weak var table: UITableView!
+    
+    // Create and initialize an empty array of string..
+    var itemsArray = [String]()
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // vieDidAppear function which loads everything for the page up before it loads.
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        // Get data out of the Core Data on the phone, saved under the key "items"
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        // If the data is able to be cast to an array of strings, save the values to variable 'temporaryItems'
+        if var temporaryItems = itemsObject as? [String] {
+            
+            // Fill the empty items array with the values from temporaryItems..
+            itemsArray = temporaryItems
+            
+        }
+
+        // Reload the data..
+        table.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +62,73 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    // *** MEET REQUIREMENTS SPECIFIED BY UITableViewDataSource PROTOCOL *** //
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Return same number of rows as items in the itemsArray
+        return itemsArray.count
+        
+    }
+    
+    
+    
+    
+    
+    
+    // Set up the cell.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Create instance of UITableViewCell, of style .default and reuseIdentifier "Cell"
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        // Set each cell's default textLabel property with the value matching the corresponding view row's indexPath.
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        
+        // Return the cell
+        return cell
+    }
+    
+    // **** //
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Make the rows deletable, using the commit editingStyle function..
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        
+        // If the recognized editingStyle is the .delete editing style.
+        if editingStyle == .delete {
+        
+            // Remove the item at that row's indexPath from the itemsArray..
+            itemsArray.remove(at: indexPath.row)
+            
+            // Reload the table's data...this should re-count the itemsArray, see that it has 1 less item, and therefore display 1 less row..
+            table.reloadData()
+            
+            // Set the updated value of the itemsArray to the key "items".
+            UserDefaults.standard.set(itemsArray, forKey: "items")
+        
+        }
+        
+    }
+    
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,11 +10,133 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    // Set up the UIClass outlets that we need for use within the code..
+    @IBOutlet weak var successfullyAddedLabel: UILabel!
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var dismissSuccessfullyAddedLabelButton: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    // Function when 'x' button is clicked on the successfullyAddedLabel bar..
+    @IBAction func dismissSuccessfullyAddedLabel(_ sender: AnyObject) {
+        
+        successfullyAddedLabel.isHidden = true
+        dismissSuccessfullyAddedLabelButton.isHidden = true
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // '+' button is pressed by user and new to do list item is to be saved...
+    @IBAction func saveItem(_ sender: AnyObject) {
+        
+        
+        // Grab the core saved data from the userDefaults which is stored under the key "items"
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        // Create an itemsArray variable, which is a class array of strings.
+        var itemsArray: [String]
+        
+        // If the objects stored within core data are convertable to an array of strings...save to a constant named 'temporaryItems'
+        if let temporaryItems = itemsObject as? [String] {
+            
+            // Fill the itemsArray up with the values stored in the temporaryItems constant
+            itemsArray = temporaryItems
+        
+            // Append the value which the user has inputted into the textField to the itemsArray..
+            itemsArray.append(userTextField.text!)
+            
+            // Show label bar which displays that adding to do list item has been successful.
+            successfullyAddedLabel.isHidden = false
+            dismissSuccessfullyAddedLabelButton.isHidden = false
+        
+        // If the file was not convertable to an array of strings..
+        } else {
+        
+            // Just set the itemsArray to be the value of the userTextField text value.
+            itemsArray = [userTextField.text!]
+        
+        }
+        
+        // Update the values stored in core data to be the same as the newly appended itemsArray.
+        UserDefaults.standard.set(itemsArray, forKey: "items")
+        
+        // Wipe the value from the userTextField so that the user can easily input another item if they need to.
+        userTextField.text = ""
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        // Set a UITapGestureRecognizer on the view itself, and if a gesture is recognizer, call the "dismissKeyboard" method.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        
+        // hide the successfullyAddedLabel and it's dismiss button at the beginning of the app loading.
+        successfullyAddedLabel.isHidden = true
+        dismissSuccessfullyAddedLabelButton.isHidden = true
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
